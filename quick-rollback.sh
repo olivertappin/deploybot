4266edfb26d63819b7feafc083cb4520f0690111
Oliver Tappin <olivertappin@gmail.com>
2016-06-08 17:22:35 +0100

    Added check to display correct output

--- quick-rollback.sh
+++ quick-rollback.sh
@@ -4,17 +4,23 @@ if [ 1 -eq "$rollback" ]; then
     base="/home/user"
     releases="$base/releases"
     commit="%COMMIT%"
+    found=false
     for release in $releases/*; do
         revision=$(cat "$release/.revision")
         if [ "$revision" = "$commit" ]; then
+            found=true
+            echo "Release found on server, switching symlink."
             rm -f "$releases/current"
             ln -s "$release" "$releases/current"
             mv -f "$releases/current" "$base/"
-            echo "Release found on server, switching symlink."
-            exit 1
+            echo "Symlink switched."
         fi
     done
-    echo "Release not found, continuing with deployment."
+    if [ "$found" = true ]; then
+        echo "Continuing with full build and deployment."
+    else
+        echo "Release not found, continuing with build and deployment."
+    fi
 else
-    echo "Deployment is not a rollback, continuing with deployment."
+    echo "Deployment is not a rollback, continuing with build and deployment."
 fi
